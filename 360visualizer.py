0b3778d0caf0ec57ded963e4912f26e5d4d2d907
uochoa <59985747+uochoa@users.noreply.github.com>
2025-04-03 14:59:50 -0700

    Update 360visualizer.py

--- 360visualizer.py
+++ 360visualizer.py
@@ -1,61 +1,125 @@
-import os
+# import os
+# import ydlidar
+# import time
+# import sys
+# from matplotlib.patches import Arc
+# import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
+# import numpy as np
+
+# RMAX = 32.0
+
+
+# fig = plt.figure()
+# lidar_polar = plt.subplot(polar=True)
+# lidar_polar.autoscale_view(True,True,True)
+# lidar_polar.set_rmax(RMAX)
+# lidar_polar.grid(True)
+# ports = ydlidar.lidarPortList();
+# port = "/dev/ydlidar";
+# for key, value in ports.items():
+#     port = value;
+    
+# laser = ydlidar.CYdLidar();
+# laser.setlidaropt(ydlidar.LidarPropSerialPort, port);
+# laser.setlidaropt(ydlidar.LidarPropSerialBaudrate, 230400);
+# laser.setlidaropt(ydlidar.LidarPropLidarType, ydlidar.TYPE_TRIANGLE);
+# laser.setlidaropt(ydlidar.LidarPropDeviceType, ydlidar.YDLIDAR_TYPE_SERIAL);
+# laser.setlidaropt(ydlidar.LidarPropScanFrequency, 10.0);
+# laser.setlidaropt(ydlidar.LidarPropSampleRate, 4);
+# laser.setlidaropt(ydlidar.LidarPropSingleChannel, False);
+# laser.setlidaropt(ydlidar.LidarPropMaxAngle, 180.0);
+# laser.setlidaropt(ydlidar.LidarPropMinAngle, -180.0);
+# laser.setlidaropt(ydlidar.LidarPropMaxRange, 16.0);
+# laser.setlidaropt(ydlidar.LidarPropMinRange, 0.02);
+# laser.setlidaropt(ydlidar.LidarPropIntenstiy, True);
+
+# scan = ydlidar.LaserScan()
+
+# def animate(num):
+    
+#     r = laser.doProcessSimple(scan);
+#     if r:
+#         angle = []
+#         ran = []
+#         intensity = []
+#         for point in scan.points:
+#             angle.append(point.angle);
+#             ran.append(point.range);
+#             intensity.append(point.intensity);
+#         lidar_polar.clear()
+#         lidar_polar.scatter(angle, ran, c=intensity, cmap='hsv', alpha=0.95)
+
+# ret = laser.initialize();
+# if ret:
+#     ret = laser.turnOn();
+#     if ret:
+#         ani = animation.FuncAnimation(fig, animate, interval=50)
+#         plt.show()
+#     laser.turnOff();
+# laser.disconnecting();
+# plt.close();
+
 import ydlidar
-import time
-import sys
-from matplotlib.patches import Arc
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
+import sys
 
-RMAX = 32.0
-
+RMAX = 16.0
 
 fig = plt.figure()
 lidar_polar = plt.subplot(polar=True)
-lidar_polar.autoscale_view(True,True,True)
 lidar_polar.set_rmax(RMAX)
 lidar_polar.grid(True)
-ports = ydlidar.lidarPortList();
-port = "/dev/ydlidar";
-for key, value in ports.items():
-    port = value;
-    
-laser = ydlidar.CYdLidar();
-laser.setlidaropt(ydlidar.LidarPropSerialPort, port);
-laser.setlidaropt(ydlidar.LidarPropSerialBaudrate, 230400);
-laser.setlidaropt(ydlidar.LidarPropLidarType, ydlidar.TYPE_TRIANGLE);
-laser.setlidaropt(ydlidar.LidarPropDeviceType, ydlidar.YDLIDAR_TYPE_SERIAL);
-laser.setlidaropt(ydlidar.LidarPropScanFrequency, 10.0);
-laser.setlidaropt(ydlidar.LidarPropSampleRate, 4);
-laser.setlidaropt(ydlidar.LidarPropSingleChannel, False);
-laser.setlidaropt(ydlidar.LidarPropMaxAngle, 180.0);
-laser.setlidaropt(ydlidar.LidarPropMinAngle, -180.0);
-laser.setlidaropt(ydlidar.LidarPropMaxRange, 16.0);
-laser.setlidaropt(ydlidar.LidarPropMinRange, 0.02);
-laser.setlidaropt(ydlidar.LidarPropIntenstiy, True);
+
+# Detect port
+ports = ydlidar.lidarPortList()
+if not ports:
+    print("No YDLIDAR ports found!")
+    sys.exit(1)
+port = next((v for v in ports.values() if "ttyUSB" in v), list(ports.values())[0])
+print(f"Using port: {port}")
+
+# Set up lidar
+laser = ydlidar.CYdLidar()
+laser.setlidaropt(ydlidar.LidarPropSerialPort, port)
+laser.setlidaropt(ydlidar.LidarPropSerialBaudrate, 230400)
+laser.setlidaropt(ydlidar.LidarPropLidarType, ydlidar.TYPE_TRIANGLE)
+laser.setlidaropt(ydlidar.LidarPropDeviceType, ydlidar.YDLIDAR_TYPE_SERIAL)
+laser.setlidaropt(ydlidar.LidarPropScanFrequency, 10.0)
+laser.setlidaropt(ydlidar.LidarPropSampleRate, 4)
+laser.setlidaropt(ydlidar.LidarPropSingleChannel, False)
+laser.setlidaropt(ydlidar.LidarPropMaxAngle, 180.0)
+laser.setlidaropt(ydlidar.LidarPropMinAngle, -180.0)
+laser.setlidaropt(ydlidar.LidarPropMaxRange, RMAX)
+laser.setlidaropt(ydlidar.LidarPropMinRange, 0.02)
+laser.setlidaropt(ydlidar.LidarPropIntensity, True)
 
 scan = ydlidar.LaserScan()
 
 def animate(num):
-    
-    r = laser.doProcessSimple(scan);
-    if r:
-        angle = []
-        ran = []
-        intensity = []
-        for point in scan.points:
-            angle.append(point.angle);
-            ran.append(point.range);
-            intensity.append(point.intensity);
+    if laser.doProcessSimple(scan):
+        angle = [p.angle for p in scan.points]
+        ran = [p.range for p in scan.points]
+        intensity = [p.intensity for p in scan.points]
         lidar_polar.clear()
-        lidar_polar.scatter(angle, ran, c=intensity, cmap='hsv', alpha=0.95)
+        lidar_polar.set_rmax(RMAX)
+        lidar_polar.scatter(angle, ran, c=intensity, cmap='hsv', alpha=0.9)
+        lidar_polar.grid(True)
 
-ret = laser.initialize();
-if ret:
-    ret = laser.turnOn();
-    if ret:
-        ani = animation.FuncAnimation(fig, animate, interval=50)
-        plt.show()
-    laser.turnOff();
-laser.disconnecting();
-plt.close();
+if laser.initialize():
+    if laser.turnOn():
+        try:
+            ani = animation.FuncAnimation(fig, animate, interval=50)
+            plt.show()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            laser.turnOff()
+            laser.disconnecting()
+    else:
+        print("Failed to turn on YDLIDAR.")
+else:
+    print("Failed to initialize YDLIDAR.")
+
