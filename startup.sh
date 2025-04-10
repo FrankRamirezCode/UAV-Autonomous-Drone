90f3c0bf1436f4f54ff9c1d7f9f8ed6779b486e0
uochoa <59985747+uochoa@users.noreply.github.com>
2025-04-10 14:19:49 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+# Activate Python virtual environment (if needed)
 source ~/myenv/bin/activate
 
-# Start MAVProxy and get GPS data
+# Start MAVProxy and get GPS data using Python
 python3 get_gps.py
 
+echo "Startup process complete!"
 
 
 
